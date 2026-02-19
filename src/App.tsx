eb24aba4aee060d0e7668a60cf244e3c327d4780
MantraCare AI <mantracare@example.com>
2026-02-19 12:41:16 +0530

    deploy: universal deployment fix (relative assets & dual routing)

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/app">
+      <BrowserRouter>
         <Routes>
-          <Route path="/calm-journal" element={<Index />} />
+          
                     
           
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
